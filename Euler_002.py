acbfb6250258fe56732c7ace9b8f34bdc8a949b2
Mohammad Mohsen <kuro.ece@gmail.com>
2018-03-08 03:10:04 +0200

    Update Euler_002.py

--- Euler_002.py
+++ Euler_002.py
@@ -13,6 +13,8 @@ def GenerateFib(limit):
         yield b
 
 def IsEven(num):
+    #& ?? bitwise AND.. All evcen numbers in binary have the LSB (bit 0) = 0
+    #so, x AND 1 will equal 0 if the number is even (LSB is 0) or 1 when odd (LSB is 1)
     return not (num & 1)
 
 print(sum(filter(IsEven, GenerateFib(4e6))))
